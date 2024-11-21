2aa3b0a572a8ddf43a09d7d761556e2a8743b5ea
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2024-11-21 22:47:57 +0630

    feat: dapper crud, efcore crud

--- DotNetBatch14KZT.ConsoleApp2/DapperExamples/DapperExample.cs
+++ DotNetBatch14KZT.ConsoleApp2/DapperExamples/DapperExample.cs
@@ -1,16 +1,95 @@
-﻿using System;
+﻿using Dapper;
+using DotNetBatch14KZT.ConsoleApp2.Dtos;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Dapper;
-using DotNetBatch14KZT.ConsoleApp2.Dtos;
-using Microsoft.Data.SqlClient;
 
 namespace DotNetBatch14KZT.ConsoleApp2.DapperExamples;
 
 public class DapperExample
 {
-    private readonly string _connectionString = AppSettings.ConnectionStringBuilder.ToString();
+    private readonly string _connectionString = AppSettings.ConnectionStringBuilder.ConnectionString;
+
+    public void Read()
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+
+        List<BlogDto> lst = connection.Query<BlogDto>("Select * from tbl_blog").ToList();
+
+        foreach (BlogDto blog in lst)
+        {
+            Console.WriteLine("id = " + blog.blog_id);
+            Console.WriteLine("title = " + blog.blog_title);
+            Console.WriteLine("author = " + blog.blog_author);
+            Console.WriteLine("content = " + blog.blog_content);
+            Console.WriteLine("");
+        }
+    }
+
+    public void Edit(string id)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+
+        var item = connection.Query<BlogDto>($"Select * from tbl_blog where blog_id='{id}'").FirstOrDefault();
+
+        if(item is null)
+        {
+            Console.WriteLine("Data not found!");
+            return;
+        }
+
+        Console.WriteLine(item.blog_id);
+        Console.WriteLine(item.blog_title);
+        Console.WriteLine(item.blog_author);
+        Console.WriteLine(item.blog_content);
+    }
+
+    public void Create(string title, string author, string content)
+    {
+        string query = @$"INSERT INTO [dbo].[tbl_blog]
+                               ([blog_title]
+                               ,[blog_author]
+                               ,[blog_content])
+                         VALUES
+                               ('{title}'
+                               ,'{author}'
+                               ,'{content}')";
+
+        using IDbConnection connection = new SqlConnection(_connectionString);
+
+        var result = connection.Execute(query);
+
+        string message = result > 0 ? "Create Success." : "Create Fail!";
+        Console.WriteLine(message);
+    }
+
+    public void Update(string id)
+    {
+        string query = $@"UPDATE [dbo].[tbl_blog]
+                                           SET [blog_title] = 'dapperUpdateTitle1'
+                                              ,[blog_author] = 'dapperUpdateAuthor1'
+                                              ,[blog_content] = 'dapperUpdateContent1'
+                                         WHERE blog_id = '{id}'";
+
+        using IDbConnection connection = new SqlConnection(_connectionString);
+
+        var result = connection.Execute(query);
+
+        string message = result > 0 ? "Update Success." : "Update Fail!";
+        Console.WriteLine(message);
+    }
+
+    public void Delete(string id)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+
+        var result = connection.Execute($"DELETE FROM [dbo].[tbl_blog] WHERE blog_id = '{id}'");
+
+        string message = result > 0 ? "Delete Success." : "Delete Fail!";
+        Console.WriteLine(message);
+    }
 }
--- DotNetBatch14KZT.ConsoleApp2/Program.cs
+++ DotNetBatch14KZT.ConsoleApp2/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System.Data;
 using DotNetBatch14KZT.ConsoleApp2.AdoDotNetExamples;
 using DotNetBatch14KZT.ConsoleApp2.DapperExamples;
+using DotNetBatch14KZT.ConsoleApp2.EFCoreExamples;
 using Microsoft.Data.SqlClient;
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
-adoDotNetExample.Read();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
 //adoDotNetExample.Edit("BF235905-D8BC-4A1D-A6C3-B797F924556D");
 //adoDotNetExample.Create("title4", "author4", "content4");
 //adoDotNetExample.Update("EC41403B-71D4-4806-A4D0-0C0226554DAC");
 //adoDotNetExample.Delete("EC41403B-71D4-4806-A4D0-0C0226554DAC");
 
-//DapperExample dapper = new DapperExample();
-//dapper.Read();
+DapperExample dapper = new DapperExample();
+dapper.Read();
+//dapper.Edit("7DB9DF38-490E-4E12-9AD2-B8E96AFC9F63");
+//dapper.Create("dapperTitle2", "dapperAuthor2", "dapperContent2");
+//dapper.Update("7DB9DF38-490E-4E12-9AD2-B8E96AFC9F63");
+//dapper.Delete("48D80173-196A-41DD-9A72-F8B7C90C3EDC");
+
+//EFCoreExample eFCoreExample = new EFCoreExample();
+//eFCoreExample.Read();
+//eFCoreExample.Edit("7DB9DF38-490E-4E12-9AD2-B8E96AFC9F63");
+//eFCoreExample.Create("eFCoreTitle", "eFCoreAuthor", "eFCoreContent");
